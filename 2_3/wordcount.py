a7397d62611c024949cf2ca48eeeef9b285d859b
Adrian Hardy Hansen <hansen.adrian.hardy@gmail.com>
2020-10-04 00:21:00 +0200

    Task 1 finished

--- 2_3/wordcount.py
+++ 2_3/wordcount.py
@@ -1,16 +1,38 @@
-import sys
-import sys
+import sys # to access command line arguments.
 
-words: dict = {}
+def word_count(filename: str) -> {}:
+    words = {}
 
-with open(sys.argv[1], "r") as file:
-    all_words = []
-    for line in file:
-        for word in line.replace(".", "").replace(",", "").replace("\n", "").replace("\t", "").split(" "):
-            if word in words:
-                words[word] += 1
-            else:
-                words[word] = 1
+    with open(sys.argv[1], "r") as file:
+        for line in file:
+            # filter each line and split by whitespace.
+            for word in line.replace(".", "").replace(",", "").replace("\n", "").replace("\t", "").split(" "): 
+                word.lower()
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+    
+    return words
 
-for word in words:
-    print(f"{word}: {words[word]}")
+def remove_rare(words: {}, min_occurences: int) -> {}:
+    # If you delete keys from the words-dict as you loop through it, you'll get a runtime-error.
+    # By making a identical copy, and deleting the values from it, you can return the copy instead, while avoiding the runtime-error.
+    result = words.copy()
+    for word in words:
+        if int(words[word]) < int(min_occurences):
+            del result[word] 
+    return result
+
+# Entry point. When calling "python wordcount.py [...]", the __name__ meta-variable of wordcount.py gets set to "__main__". 
+if __name__ == "__main__":
+    if len(sys.argv) == 2: # If only filename is provided.
+        words = word_count(sys.argv[1])
+        print(words)
+    elif len(sys.argv) == 3: # If filename and min_occurences are provided.
+        words = word_count(sys.argv[1])
+        words = remove_rare(words, sys.argv[2])
+        print(words)
+    else:
+        print("Please provide a filename, and an optional number for min occurences in that order.")
+
