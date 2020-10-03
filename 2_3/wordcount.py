37a037ebef9140510a3354e145f9e449dd625f6a
Adrian Hardy Hansen <hansen.adrian.hardy@gmail.com>
2020-10-04 00:24:15 +0200

    Task 1. Refactored.

--- 2_3/wordcount.py
+++ 2_3/wordcount.py
@@ -1,7 +1,21 @@
 import sys # to access command line arguments.
 
+# Feels more natural for remove_rare to be an extension-method, so I extended the dict-class.
+class Words(dict):
+    def __init__(self, iterable):
+        super().__init__(iterable)
+
+    def remove_rare(self, min_occurences: int) -> {}:
+        # If you delete keys from the words-dict as you loop through it, you'll get a runtime-error.
+        # By making a identical copy, and deleting the values from it, you can return the copy instead, while avoiding the runtime-error.
+        result = self.copy()
+        for word in self:
+            if int(self[word]) < int(min_occurences):
+                del result[word] 
+        return result
+
 def word_count(filename: str) -> {}:
-    words = {}
+    words = Words({})
 
     with open(sys.argv[1], "r") as file:
         for line in file:
@@ -15,23 +29,13 @@ def word_count(filename: str) -> {}:
     
     return words
 
-def remove_rare(words: {}, min_occurences: int) -> {}:
-    # If you delete keys from the words-dict as you loop through it, you'll get a runtime-error.
-    # By making a identical copy, and deleting the values from it, you can return the copy instead, while avoiding the runtime-error.
-    result = words.copy()
-    for word in words:
-        if int(words[word]) < int(min_occurences):
-            del result[word] 
-    return result
-
 # Entry point. When calling "python wordcount.py [...]", the __name__ meta-variable of wordcount.py gets set to "__main__". 
 if __name__ == "__main__":
     if len(sys.argv) == 2: # If only filename is provided.
         words = word_count(sys.argv[1])
         print(words)
     elif len(sys.argv) == 3: # If filename and min_occurences are provided.
-        words = word_count(sys.argv[1])
-        words = remove_rare(words, sys.argv[2])
+        words = word_count(sys.argv[1]).remove_rare(sys.argv[2])
         print(words)
     else:
         print("Please provide a filename, and an optional number for min occurences in that order.")
